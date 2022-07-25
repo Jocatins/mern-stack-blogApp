d381ea3a78785db46f1149dcb2fb37e2b03a4384
Jocatins <jocakhamen@gmail.com>
2022-07-25 18:43:58 +0100

    mern-stack-with-typescript

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+const config = {
+    firebase: {
+        apiKey: 'AIzaSyDf_BLCWDHnL-ip4QGO0wE8lD5ox2EFFo8',
+        authDomain: 'sphinx-blog.firebaseapp.com',
+        databaseURL: 'https://sphinx-blog.firebaseio.com',
+        projectId: 'sphinx-blog',
+        storageBucket: 'sphinx-blog.appspot.com',
+        messagingSenderId: '233477110305',
+        appId: '1:233477110305:web:62b1e7d62003f5b3504160',
+        measurementId: 'G-Q0503N9T5M'
+    },
+    server: {
+        url: 'http://localhost/1618'
+    }
+};
+export default config;
